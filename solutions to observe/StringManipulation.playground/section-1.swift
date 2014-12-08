c144fd9d66302c6050cc320e716630c436ab353c
David Fisher <fisherds@rose-hulman.edu>
2014-12-08 09:54:00 -0500

    String to Character Array

--- solutions to observe/StringManipulation.playground/section-1.swift
+++ solutions to observe/StringManipulation.playground/section-1.swift
@@ -2,16 +2,15 @@ import Foundation
 //import UIKit
 
 let mice = "🐭🐭🐭🐭🐭"
-let cat = "🐯"
+let cat = "🐱"
 let dog : Character = "🐶"
 var myStr = "My \(cat) loves to chase \(mice) around the 🏡"
 
-countElements(mice)
-countElements(myStr)
 
 // Get a Character or range substring
 myStr[advance(myStr.startIndex, 3)]
 myStr.substringWithRange(advance(myStr.startIndex, 3)...advance(myStr.startIndex, 20))
+myStr.substringFromIndex(advance(myStr.startIndex, 24))
 
 // Inserting and removing characters
 myStr.splice([dog], atIndex: advance(myStr.startIndex, 3))
@@ -19,8 +18,15 @@ myStr.removeAtIndex(advance(myStr.startIndex, 4))
 myStr
 myStr.removeRange(advance(myStr.startIndex, 10)...advance(myStr.startIndex, 31))
 
-// Strings to arrays of strings
+// String to arrays of Strings broken by spaces (and back).
 var myArray = myStr.componentsSeparatedByString(" ")
 myArray[3] = "my"
-"___".join(myArray)
+myStr = " ".join(myArray)
+
+// String to an array of Characters (and back).
+var stringAsCharacterArray = Array(myStr)
+stringAsCharacterArray[stringAsCharacterArray.count - 1] = "🐱"
+stringAsCharacterArray
+myStr = String(stringAsCharacterArray)
+
 
