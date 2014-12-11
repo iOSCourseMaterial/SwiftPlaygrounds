6648656bd9a0b45de86616e920d081019a264d0a
David Fisher <fisherds@rose-hulman.edu>
2014-12-11 16:28:01 -0500

    Add Closures function calls

--- follow alongs/Closures.playground/section-1.swift
+++ follow alongs/Closures.playground/section-1.swift
@@ -15,3 +15,13 @@ var values = [10, 5, 2, 8, 3, 6, 1, 1000]
 
 // A Closure using a closure for a variable
 
+
+
+
+
+
+
+
+
+//numValuesOverLimit(values, limit: 5)
+//numValuesOverLimit(values, limit: 9)
