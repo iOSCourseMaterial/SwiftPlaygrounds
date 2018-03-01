f52d79815b4aac2c8b6713197eebf14084414b96
David Fisher <fisherds@rose-hulman.edu>
2018-03-01 14:12:26 -0500

    fix LinearLightsOut demo code

--- lights out lab/LinearLightsOutGame.playground/contents.swift
+++ lights out lab/LinearLightsOutGame.playground/contents.swift
@@ -4,16 +4,27 @@
 
 /* ----------------- Official Playground testing ----------------- */
 //var lg = LinearLightsOutGame(numLights: 13)
-//lg.lightStates = [Bool](count: 13, repeatedValue: true)
+//lg.lightStates = [Bool](repeating: true, count: 13)
 //lg.pressedLightAtIndex(0)
+//lg
 //lg.pressedLightAtIndex(3)
+//lg
 //lg.pressedLightAtIndex(6)
+//lg
 //lg.pressedLightAtIndex(9)
+//lg
 //lg.pressedLightAtIndex(12)
+//lg
 //lg.pressedLightAtIndex(1)
-
+//lg
+//
+//
 //var lg2 = LinearLightsOutGame(numLights: 13)
 //lg2.lightStates = [true, true, false, false, false, false, false, false, false, false, true, true, true]
 //lg2.pressedLightAtIndex(0)
+//lg2
 //lg2.pressedLightAtIndex(11)
+//lg2
 //lg2.pressedLightAtIndex(6)
+//lg2
+
