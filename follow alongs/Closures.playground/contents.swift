f330907e1d2baad39d4e056eda9bf0c33745cfd9
David Fisher <fisherds@rose-hulman.edu>
2018-03-01 12:52:58 -0500

    adding a func vs closure syntax comparison

--- follow alongs/Closures.playground/contents.swift
+++ follow alongs/Closures.playground/contents.swift
@@ -25,3 +25,14 @@ var values = [10, 5, 2, 8, 3, 6, 1, 1000]
 
 //numValuesOverLimit(values, limit: 5)
 //numValuesOverLimit(values, limit: 9)
+
+
+// Comparing func and closure syntax
+func asAFunc(_ someString: String, _ someNumber: Int) -> String {
+  return String(repeating: someString, count: someNumber)
+}
+let asAClosure = {(_ someString: String, _ someNumber: Int) -> String in
+  return String(repeating: someString, count: someNumber)
+}
+asAFunc("Holy ", 3)
+asAClosure("Holy ", 3)
