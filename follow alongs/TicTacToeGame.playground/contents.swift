2a8e331bf0d09accfe1b4deb2db1d2a2c39730e5
David Fisher <fisherds@rose-hulman.edu>
2018-03-01 14:02:06 -0500

    Removed calls to description since that bug is fixed

--- follow alongs/TicTacToeGame.playground/contents.swift
+++ follow alongs/TicTacToeGame.playground/contents.swift
@@ -6,34 +6,24 @@
 
 /* ----------------- Official Playground testing ----------------- */
 //var game = TicTacToeGame()
-//game.description
 //game.pressedSquare(0)
-//game.description
 //game.pressedSquare(1)
-//game.description
 //game.pressedSquare(3)
-//game.description
 //game.pressedSquare(2)
-//game.description
 //game.pressedSquare(6)
-//game.description
 
 
 //var game2 = TicTacToeGame()
-//game2.gameBoard = [.X, .X, .O,
-//                .None, .None, .None,
-//                .O, .None, .None]
-//game2.description
+//game2.gameBoard = [.x, .x, .o,
+//                .none, .none, .none,
+//                .o, .none, .none]
 //game2.pressedSquare(8)
-//game2.description
 //game2.pressedSquare(4)
-//game2.description
 
 
 //var game3 = TicTacToeGame()
-//game3.gameBoard = [.X, .X, .O,
-//    .O, .O, .X,
-//    .X, .O, .None]
-//game3.description
+//game3.gameBoard = [.x, .x, .o,
+//    .o, .o, .x,
+//    .x, .o, .none]
 //game3.pressedSquare(8)
-//game3.description
+
