d80ac5639fa421c84d25fb3fd678446de87198e0
David Fisher <fisherds@rose-hulman.edu>
2020-12-04 13:21:43 -0500

    variable renames

--- ticTacToeGame/TicTacToeGame.playground/contents.swift
+++ ticTacToeGame/TicTacToeGame.playground/contents.swift
@@ -6,24 +6,24 @@
 
 /* ----------------- Official Playground testing ----------------- */
 //var game = TicTacToeGame()
-//game.pressedSquare(0)
-//game.pressedSquare(1)
-//game.pressedSquare(3)
-//game.pressedSquare(2)
-//game.pressedSquare(6)
+//game.pressedSquareAt(0)
+//game.pressedSquareAt(1)
+//game.pressedSquareAt(3)
+//game.pressedSquareAt(2)
+//game.pressedSquareAt(6)
 
 
 //var game2 = TicTacToeGame()
-//game2.gameBoard = [.x, .x, .o,
+//game2.board = [.x, .x, .o,
 //                .none, .none, .none,
 //                .o, .none, .none]
-//game2.pressedSquare(8)
-//game2.pressedSquare(4)
+//game2.pressedSquareAt(8)
+//game2.pressedSquareAt(4)
 
 
 //var game3 = TicTacToeGame()
-//game3.gameBoard = [.x, .x, .o,
+//game3.board = [.x, .x, .o,
 //    .o, .o, .x,
 //    .x, .o, .none]
-//game3.pressedSquare(8)
+//game3.pressedSquareAt(8)
 
